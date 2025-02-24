5b5ee80de008db56eb2e4771d491a72bcb0b3332
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-02-24 09:30:39 -0300

    first commit

---
+++ src/main/java/com/laisa/callSystem/CallSystemApplication.java
@@ -0,0 +1,13 @@
+package com.laisa.callSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laisa/callSystem/CallSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laisa.callSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
