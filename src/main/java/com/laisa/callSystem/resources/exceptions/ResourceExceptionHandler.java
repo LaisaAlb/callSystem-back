570ebef801bda88bd2d9e9906ba5fe16db3e134e
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-02-26 11:04:27 -0300

    Exceção para Objetos não encontrados

---
+++ src/main/java/com/laisa/callSystem/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.laisa.callSystem.resources.exceptions;
+
+import com.laisa.callSystem.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+        StandardError error = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Object Not Found",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
--- src/main/java/com/laisa/callSystem/services/TecnicoService.java
+++ src/main/java/com/laisa/callSystem/services/TecnicoService.java
@@ -2,6 +2,7 @@ package com.laisa.callSystem.services;
 
 import com.laisa.callSystem.domain.Tecnico;
 import com.laisa.callSystem.repositories.TecnicoRepository;
+import com.laisa.callSystem.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,12 @@ import java.util.Optional;
 
 @Service
 public class TecnicoService {
+
     @Autowired
     private TecnicoRepository tecnicoRepository;
 
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = tecnicoRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
     }
 }
