ada289c5fedcdc6191cb8800e1c9fca59b0633c4
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-02-27 16:35:17 -0300

    Delete para técnico

--- src/main/java/com/laisa/callSystem/services/TecnicoService.java
+++ src/main/java/com/laisa/callSystem/services/TecnicoService.java
@@ -46,6 +46,15 @@ public class TecnicoService {
         return tecnicoRepository.save(oldObj);
     }
 
+    public void delete(Integer id){
+        Tecnico obj = findById(id);
+        if(obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+        }
+
+        tecnicoRepository.delete(obj);
+    }
+
     private void validaPorCpfEEmail(TecnicoDTO objDTO){
         Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
         // Esse método serve tanto para validar a criação de um novo técnico, quanto para atualizar
