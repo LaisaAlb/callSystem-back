3e1243bcc6232514bb64df4d8835d5ef13f8cfa3
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-03-05 14:07:44 -0300

    Criacao de Autenticacao e Autorizacao do JWT - Corrigido

--- src/main/java/com/laisa/callSystem/config/SecurityConfig.java
+++ src/main/java/com/laisa/callSystem/config/SecurityConfig.java
@@ -18,8 +18,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
+
 
 @Configuration
 @EnableMethodSecurity
@@ -67,6 +67,7 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
